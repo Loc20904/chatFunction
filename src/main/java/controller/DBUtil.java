fd33d95f561db89a80ca91a3997a7fd1e319cb9c
winhtuan <winhtuan@gmail.com>
2025-05-22 14:36:41 +0700

    fix db 2

--- src/main/java/controller/DBUtil.java
+++ src/main/java/controller/DBUtil.java
@@ -9,18 +9,18 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
+
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:sqlserver://database-1.cfwc8w0ey4aa.ap-southeast-1.rds.amazonaws.com:1433;databaseName=chat;encrypt=false;trustServerCertificate=true;loginTimeout=30;";
         String user = "sa";
         String password = "Tuan210604";
         try {
-    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-} catch (ClassNotFoundException e) {
-    e.printStackTrace();
-}
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return DriverManager.getConnection(url, user, password);
     }
 
 }
-
