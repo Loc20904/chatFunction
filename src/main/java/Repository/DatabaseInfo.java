9f04a200c0a1f02a8e77c5151a78be9d26bde033
Nguyễn Minh Tuấn <114925058+winhtuan@users.noreply.github.com>
2025-06-03 15:07:56 +0700

    Update DatabaseInfo.java

--- src/main/java/Repository/DatabaseInfo.java
+++ src/main/java/Repository/DatabaseInfo.java
@@ -6,7 +6,7 @@ public interface DatabaseInfo {
     // LAPTOP-0L8RQE0C loc    --- LOC  
     public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
     public static String DBURL="jdbc:sqlserver:chat.cfwc8w0ey4aa.ap-southeast-1.rds.amazonaws.com:1433;databaseName=ChatAppDB;encrypt=false;trustServerCertificate=true;loginTimeout=30;";
-    public static String USERDB="sa";
-    public static String PASSDB="123";
+    public static String USERDB="admin";
+    public static String PASSDB="Tuan210604";
    
 }
