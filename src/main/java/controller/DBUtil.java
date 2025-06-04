9bf7aef7ae682b4052b4aefaae3cade4b26b6759
Nguyễn Minh Tuấn <114925058+winhtuan@users.noreply.github.com>
2025-06-04 14:48:01 +0700

    Update DBUtil.java

--- src/main/java/controller/DBUtil.java
+++ src/main/java/controller/DBUtil.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBUtil {
 
     public static Connection getConnection() throws SQLException {
-        String url = "jdbc:mysql://chat.cfwc8w0ey4aa.ap-southeast-1.rds.amazonaws.com:3306/ChatAppDB?useSSL=false&serverTimezone=UTC";
+        String url = "jdbc:mysql://ws.cfwc8w0ey4aa.ap-southeast-1.rds.amazonaws.com:3306/ChatAppDB?useSSL=false&serverTimezone=UTC";
 
         String user = "admin";
         String password = "Tuan210604";
